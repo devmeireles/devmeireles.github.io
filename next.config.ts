71ac3b7f72017c176bc7454bbc05d3f4b855fd88
Gabriel Meireles <meireleswebdesign@gmail.com>
2025-04-23 17:48:18 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: process.env.PAGES_BASE_PATH,
   images: {
     unoptimized: true
   }
